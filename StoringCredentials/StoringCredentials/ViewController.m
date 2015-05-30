80fc6b8b7edaf50e7018a5b07a2089b3c87ee849
Bryan Luby <bluby74@gmail.com>
2015-05-30 10:32:28 -0500

    Update credential storage methods

--- StoringCredentials/StoringCredentials/ViewController.m
+++ StoringCredentials/StoringCredentials/ViewController.m
@@ -17,25 +17,67 @@
 
 @implementation ViewController
 
-- (IBAction)saveButtonPressed:(id)sender
+- (void)viewDidLoad
 {
-    [self saveCredentials];
+    [super viewDidLoad];
+    
+    [self populateCredential];
 }
 
-- (void)saveCredentials
+- (IBAction)saveButtonPressed:(id)sender
 {
-    NSURLCredential *credential = [NSURLCredential credentialWithUser:self.usernameTextField.stringValue
-                                                             password:self.passwordTextField.stringValue
-                                                          persistence:NSURLCredentialPersistencePermanent];
+    [self saveDefaultCredential];
+}
+
+- (NSURLProtectionSpace *)protectionSpace
+{
+    static dispatch_once_t onceToken;
+    static NSURLProtectionSpace *_protectionSpace;
     
-    NSURLProtectionSpace *protectionSpace = [[NSURLProtectionSpace alloc] initWithHost:@"www.example.com"
-                                                                                  port:0
-                                                                              protocol:@"http"
-                                                                                 realm:nil
-                                                                  authenticationMethod:nil];
+    dispatch_once(&onceToken, ^{
+        _protectionSpace = [[NSURLProtectionSpace alloc] initWithHost:@"www.example.com"
+                                                                 port:0
+                                                             protocol:@"http"
+                                                                realm:nil
+                                                 authenticationMethod:nil];
+    });
     
-    [[NSURLCredentialStorage sharedCredentialStorage] setCredential:credential
-                                                 forProtectionSpace:protectionSpace];
+    return _protectionSpace;
+}
+
+- (void)populateCredential
+{
+    NSURLCredential *defaultCredential = [[NSURLCredentialStorage sharedCredentialStorage]
+                                          defaultCredentialForProtectionSpace:[self protectionSpace]];
+    
+    if (defaultCredential.user && defaultCredential.hasPassword) {
+        self.usernameTextField.stringValue = defaultCredential.user;
+        self.passwordTextField.stringValue = defaultCredential.password;
+    }
+    
+    NSLog(@"\n--- Credentials ---\nUsername: %@\nPassword: %@", defaultCredential.user, defaultCredential.password);
+}
+
+- (void)saveDefaultCredential
+{
+    [self removePreviousDefaultCredential];
+    
+    NSURLCredential *newDefaultCredential = [NSURLCredential credentialWithUser:self.usernameTextField.stringValue
+                                                                       password:self.passwordTextField.stringValue
+                                                                    persistence:NSURLCredentialPersistencePermanent];
+    
+    [[NSURLCredentialStorage sharedCredentialStorage] setDefaultCredential:newDefaultCredential
+                                                        forProtectionSpace:[self protectionSpace]];
+}
+
+- (void)removePreviousDefaultCredential
+{
+    NSURLCredential *previousDefaultCredential = [[NSURLCredentialStorage sharedCredentialStorage]
+                                                  defaultCredentialForProtectionSpace:[self protectionSpace]];
+    
+    [[NSURLCredentialStorage sharedCredentialStorage]
+     removeCredential:previousDefaultCredential
+     forProtectionSpace:[self protectionSpace]];
 }
 
 @end
