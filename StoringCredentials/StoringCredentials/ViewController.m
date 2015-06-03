473ba8a9be5902205eb5ce2741ba97fd3c2b9f1f
Bryan Luby <bluby74@gmail.com>
2015-06-03 08:16:12 -0500

    Test auth challenge

--- StoringCredentials/StoringCredentials/ViewController.m
+++ StoringCredentials/StoringCredentials/ViewController.m
@@ -8,10 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <NSURLSessionTaskDelegate>
 
 @property (nonatomic, weak) IBOutlet NSTextField *usernameTextField;
 @property (nonatomic, weak) IBOutlet NSTextField *passwordTextField;
+@property (nonatomic, strong) NSURLSession *session;
+@property (nonatomic, strong) NSURLSessionTask *task;
 
 @end
 
@@ -22,6 +24,10 @@
     [super viewDidLoad];
     
     [self populateCredential];
+
+    self.session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]
+                                                 delegate:self
+                                            delegateQueue:[NSOperationQueue mainQueue]];
 }
 
 - (IBAction)saveButtonPressed:(id)sender
@@ -29,6 +35,18 @@
     [self saveDefaultCredentialForUsername:self.usernameTextField.stringValue
                                   password:self.passwordTextField.stringValue
                            protectionSpace:[self protectionSpace]];
+    [self testChallenge];
+}
+
+- (void)testChallenge
+{
+    NSString *urlString =  @"https://www.instapaper.com/api/authenticate";
+
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    request.HTTPMethod = @"POST";
+
+    self.task = [self.session dataTaskWithRequest:request];
+    [self.task resume];
 }
 
 - (NSURLProtectionSpace *)protectionSpace
@@ -57,7 +75,7 @@
         self.passwordTextField.stringValue = defaultCredential.password;
     }
     
-    NSLog(@"\n--- Credentials ---\nUsername: %@\nPassword: %@", defaultCredential.user, defaultCredential.password);
+//    NSLog(@"\n--- Credentials ---\nUsername: %@\nPassword: %@", defaultCredential.user, defaultCredential.password);
 }
 
 - (void)saveDefaultCredentialForUsername:(NSString *)username
@@ -83,6 +101,27 @@
         [[NSURLCredentialStorage sharedCredentialStorage] removeCredential:previousDefaultCredential
                                                         forProtectionSpace:protectionSpace];
     }
+}
+
+#pragma mark - NSURLSessionTaskDelegate
+
+-  (void)URLSession:(NSURLSession *)session
+didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+  completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition, NSURLCredential *))completionHandler
+{
+    NSURLCredential *defaultCredential = [[NSURLCredentialStorage sharedCredentialStorage]
+                                          defaultCredentialForProtectionSpace:[self protectionSpace]];
+
+    if (defaultCredential.user && defaultCredential.hasPassword) {
+        completionHandler(NSURLSessionAuthChallengeUseCredential, defaultCredential);
+    } else {
+        completionHandler(NSURLSessionAuthChallengeCancelAuthenticationChallenge, nil);
+    }
+}
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
+{
+
 }
 
 @end
